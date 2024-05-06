072d71bd2980efe79449aad159cf551bdee52ef9
DinkyInkyDog <142037259+DinkyInkyDog@users.noreply.github.com>
2024-05-06 05:34:27 -0400

    setup player

--- src/cardGameWar/Player.java
+++ src/cardGameWar/Player.java
@@ -1,5 +1,28 @@
 package cardGameWar;
 
-public class Player {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Player {
+	List<Card> hand = new ArrayList<Card>();
+	private int score;
+	private String name;
+	
+	public Player(String name) {
+		this.score = 0;
+		this.name = name;
+	}
+	
+	
+	public int getScore() {
+		return this.score;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+	
+	
 }
