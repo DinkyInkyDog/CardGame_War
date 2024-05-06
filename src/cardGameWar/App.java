c20ad5eb4fc4e8df05804479d4cb8abc44d36487
DinkyInkyDog <142037259+DinkyInkyDog@users.noreply.github.com>
2024-05-06 06:35:41 -0400

    add flip function and edit draw card

--- src/cardGameWar/App.java
+++ src/cardGameWar/App.java
@@ -1,14 +1,24 @@
 package cardGameWar;
 
-public class Program {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+public class App {
 
 	public static void main(String[] args) {
 		// lets test this out
 		//first I'll need two characters
 		
+		Deck playDeck = new Deck();
+		playDeck.shuffle();
+		
 		Player p1 = new Player("Lauren");
 		Player p2 = new Player("Molly");
-
+		
+		
+	
 	}
-
+	
 }
--- src/cardGameWar/Player.java
+++ src/cardGameWar/Player.java
@@ -30,6 +30,26 @@ public class Player {
 		for (Card card: hand) {
 			card.describe();
 		}
+		
+	}
+	public void drawToHand(Deck currentDeck) {
+		Card pulledCard = currentDeck.draw();
+		hand.add(pulledCard);
+		
 	}
 	
+	public int flip() {
+		int cardPoints;
+		Card pulledCard = hand.get(0);
+		hand.remove(0);
+		cardPoints = pulledCard.getValue();
+		System.out.print(name + " plays ");
+		pulledCard.describe();
+		return cardPoints;
+		}
+	
+	public void incrementScore() {
+		System.out.println(name + " won the battle!");
+		this.score += 1;
+	}
 }
