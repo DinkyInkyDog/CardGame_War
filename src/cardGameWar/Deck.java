6fb003bd30f0438fa27b7abd3a373ce07288acc5
DinkyInkyDog <142037259+DinkyInkyDog@users.noreply.github.com>
2024-05-06 05:30:30 -0400

    recreate shuffle method
    I figured out how to do this during week 5's coding project so I just reused my method from that.

--- src/cardGameWar/Deck.java
+++ src/cardGameWar/Deck.java
@@ -3,6 +3,7 @@ package cardGameWar;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 public class Deck {
 	List<Card> newDeck = new ArrayList<Card>();
@@ -16,9 +17,28 @@ public class Deck {
 			}
 		}
 	}
+	public int pickACard(int min, int max) {
+	    Random random = new Random();
+	    return random.nextInt(max - min) + min;
+	}
 	
 	public void shuffle() {
 		List<Card> tempDeck = new ArrayList<Card>();
-		
+		for (int i = newDeck.size(); i > 1; i--) {
+			int activeCard = pickACard(0, i - 1);
+			tempDeck.add(newDeck.get(activeCard));
+			newDeck.remove(activeCard);
+		}
+		for(Card card: tempDeck) {
+			newDeck.add(card);
+		}
+	}
+	
+	
+	public Card draw() {
+		Card pulledCard = newDeck.get(0);
+		newDeck.remove(0);
+		return pulledCard;
 	}
 }
+
